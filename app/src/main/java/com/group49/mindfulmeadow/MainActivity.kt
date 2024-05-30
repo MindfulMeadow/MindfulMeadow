3cb944fc93857ac9bd1ce47a55f62b053a56e533
lfengzuo <754198932@qq.com>
2024-05-30 18:26:38 +0100

    ADD: add the BottomBar UI into MainActivity

--- app/src/main/java/com/group49/mindfulmeadow/MainActivity.kt
+++ app/src/main/java/com/group49/mindfulmeadow/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.mindfulmeadow.R
 import com.group49.mindfulmeadow.ui.theme.MindfulMeadowTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +29,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MindfulMeadowTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Column {
+                   // BottomBar()
                 }
             }
         }
@@ -31,17 +38,35 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+private fun BottomBar(selected: Int) {
+    Row {
+        TabItem(if (selected == 0) R.drawable.baseline_grass_24_green else R.drawable.baseline_grass_24_white, "Home", Modifier.weight(1f))
+        TabItem(if (selected == 1) R.drawable.baseline_history_24_green else R.drawable.baseline_history_24_white, "History", Modifier.weight(1f))
+        TabItem(if (selected == 2) R.drawable.baseline_calendar_month_24_green else R.drawable.baseline_calendar_month_24_white, "Graph", Modifier.weight(1f))
+        TabItem(if (selected == 3) R.drawable.baseline_analytics_24_green else R.drawable.baseline_analytics_24_white, "Analysis", Modifier.weight(1f))
+    }
+}
+
+@Composable
+private fun TabItem(@DrawableRes iconId: Int, title : String, modifier: Modifier = Modifier) {
+    Column(
+        modifier.padding(vertical = 8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        Icon(painterResource(iconId),
+            contentDescription = title)
+        Text(text = title,
+            fontSize = 11.sp)
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    MindfulMeadowTheme {
-        Greeting("Android")
-    }
+fun BottomBarPreview() {
+    BottomBar(selected = 1)
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TabItemPreview() {
+    TabItem(iconId = R.drawable.baseline_grass_24_white, title = "Home")
 }
